a4254111447bfdea6063ca2286c9b15c9fbe3e7f
Carlos <carlosguillermoru@outlook.com>
2019-11-26 11:16:46 +0100

    Creación de la clase principal

---
+++ src/main/java/daw/carlos/VentanaPrincipal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.carlos;
+
+import javax.swing.JFrame;
+/**
+ *
+ * @author carlos-pc
+ */
+
+//Clase que hereda de JFrame
+//Automáticamente la clase VentanaPrincipal tiene todos
+//Los atributos y métodos de JFrame
+public class VentanaPrincipal extends JFrame{
+    
+}
