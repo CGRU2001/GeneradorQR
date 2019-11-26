032f4d57b7d4cb9d2f8d90680264a787e9e1aec8
Carlos <carlosguillermoru@outlook.com>
2019-11-26 12:23:53 +0100

    Creamos el panel

---
+++ src/main/java/daw/carlos/Panel.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.carlos;
+
+import javax.swing.JPanel;
+/**
+ *
+ * @author carlos-pc
+ */
+public class Panel extends JPanel{
+    //Ajustamos las constantes del tamaño del panel
+    public static final int ANCHO_FRAME = 600;     
+    public static final int ALTO_FRAME = 200;
+}
