b6680280b8fc6a9cdce481a26db275762f1ed357
Carlos <carlosguillermoru@outlook.com>
2019-11-28 12:05:18 +0100

    Crea el método escribirQR

--- src/main/java/daw/carlos/QR.java
+++ src/main/java/daw/carlos/QR.java
@@ -5,6 +5,21 @@
  */
 package daw.carlos;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+//import java.util.logging.Level;
+//import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author carlos-pc
@@ -12,4 +27,33 @@ package daw.carlos;
 public class QR {
     public static final int ANCHO_QR = 400;
     public static final int ALTO_QR = 400;
+    
+    
+    //Este método genera un archivo en el disco duro con el nombre que se
+    //indica en la variable "fichero". El texto que codifica está contenido en 
+    //"texto" y el formato de la imagen puede ser jpeg, tiff, gif, png...
+    
+    
+    public static void escribirQR(String texto, String fichero, String formatoImagen){
+        //Objeto QRCodeWriter 
+        //Renderiza un código QR como una matriz de dos dimensiones (BitMatrix)
+        QRCodeWriter qrcw = new QRCodeWriter();
+        
+        BitMatrix matrizPuntos = null;
+        //FileOutputSream escribe en un fichero un flujo de bytes, como una imagen
+        FileOutputStream ficheroSalida = null;
+        
+        try {
+            matrizPuntos = qrcw.encode(texto, BarcodeFormat.QR_CODE, ANCHO_QR, ALTO_QR);
+            ficheroSalida = new FileOutputStream(new File(fichero));
+            MatrixToImageWriter.writeToStream(matrizPuntos, "png", ficheroSalida);
+            JOptionPane.showMessageDialog(null, "Se ha generado la imagen " + fichero);
+        } catch (WriterException ex) {
+            JOptionPane.showMessageDialog(null, "El contenido no pudo ser codificado");
+        } catch (FileNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, "No se pudo generar el fichero");
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(null, "No se puede crear el archivo con el QR");
+        }
+    }
 }
