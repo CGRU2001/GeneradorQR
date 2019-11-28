0da9a5390c663478d70221e3be335d87ddce1c61
Carlos <carlosguillermoru@outlook.com>
2019-11-28 11:01:40 +0100

    Añade clase QR

---
+++ src/main/java/daw/carlos/QR.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.carlos;
+
+/**
+ *
+ * @author carlos-pc
+ */
+public class QR {
+    public static final int ANCHO_QR = 400;
+    public static final int ALTO_QR = 400;
+}
